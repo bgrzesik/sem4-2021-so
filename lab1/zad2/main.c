fcad06d31f9d7ee249a36b7363e876684a644a98
Bartłomiej Grzesik <grzechovsky@gmail.com>
2021-03-16 01:53:48 +0100

    Draft task 2

---
+++ lab1/zad2/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+const char *
+consume(int *argc, const char ***argv)
+{
+    if (*argc <= 0) {
+        return NULL;
+    }
+
+    (*argc)--;
+    const char *ret = *argv[0];
+
+    *argv = &(*argv)[1];
+
+    return ret;
+}
+
+int
+cmd_create_table(int *argc, const char ***argv) {
+    const char *arg = consume(argc, argv);
+    
+    if (arg == NULL) {
+        return -1;
+    }
+
+    size_t size = strtoull(arg, NULL, 10);
+
+    printf("create_table %ld \n", size);
+}
+
+
+struct _cmd_t
+{
+    const char *name;
+    int (*func)(int *, const char ***);
+};
+
+struct _cmd_t cmds[] = {
+    { "create_table", &cmd_create_table }
+};
+
+int 
+main(int argc, const char **argv) 
+{
+    const char *cmd = consume(&argc, &argv);
+    while (cmd != NULL) {
+
+        for (int i = 0; i < sizeof(cmds) / sizeof(cmds[0]); i++) {
+            if (strcmp(cmd, cmds[i].name) == 0) {
+                int ret = cmds[i].func(&argc, &argv);
+                
+                if (ret != 0) {
+                    return ret;
+                }
+            }
+        }
+
+
+
+        cmd = consume(&argc, &argv);
+    }
+
+
+    return 0;
+}
